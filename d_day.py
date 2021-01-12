6ff07fd67a3fbc147d80d9278c88cc0982f2d0df
hwangbo98 <hwangbo1998@instance-1.asia-northeast3-a.c.neon-particle-300409.internal>
2021-01-12 14:54:23 +0000

    night, early, day, free

--- d_day.py
+++ d_day.py
@@ -9,21 +9,29 @@ goal_day = input("you should type the day ex) 1970-01-01 : ")
 goal_day_s = datetime.strptime(goal_day, "%Y-%m-%d")
 
 delta = goal_day_s - now_i
-print( "%s day" %(delta.days))
+print( "%s day left!" %(delta.days))
 deltaToint = delta.days
-if deltaToint%3 == 0 :
-	day_time = deltaToint//3
-	night_time = deltaToint//3
-	first_night = deltaToint//3
-elif deltaToint%3 == 1 :
-	day_time = deltaToint//3 + 1
-	first_night = deltaToint//3 
-	night_time = deltaToint//3
+if deltaToint%4 == 0 :
+	day_time = deltaToint//4
+	night_time = deltaToint//4
+	first_night = deltaToint//4
+	free_time = deltaToint//4
+elif deltaToint%4 == 1 : 
+	day_time = deltaToint//4 + 1
+	first_night = deltaToint//4 
+	night_time = deltaToint//4
+	free_time = deltaToint//4
+elif deltaToint%4 == 2 : 
+	day_time = deltaToint//4 + 1
+	first_night = deltaToint//4 
+	night_time = deltaToint//4
+	free_time = deltaToint//4 + 1
 else :
-	day_time = deltaToint//3 + 1
-	first_night = deltaToint//3 + 1
-	night_time = deltaToint//3
+	day_time = deltaToint//4 + 1
+	first_night = deltaToint//4 + 1
+	night_time = deltaToint//4 + 1
+	free_time = deltaToint//4
 
 print(day_time) 
-print(" you have {}nighttime, {}firstnight, {}daytime".format(night_time,first_night,day_time))
+print("you have {}nighttime, {}firstnight, {}daytime, {}freetime".format(night_time,first_night,day_time,free_time))
 print(type(deltaToint))
