427dc9aed6d3b4a4d399acbbc4b82231933e4b7e
hwangbo98 <hwangbo1998@instance-1.asia-northeast3-a.c.neon-particle-300409.internal>
2021-01-12 06:23:22 +0000

    d_day & how many

--- d_day.py
+++ d_day.py
@@ -1,18 +1,29 @@
 from datetime import *
-from dateutil.relativedelta import *
 from pytz import timezone
 
 now = datetime.now(timezone('Asia/Seoul'))
 now_s = now.strftime("%Y-%m-%d")
 now_i = datetime.strptime(now_s, "%Y-%m-%d")
-#year, month, day = 2021, 4, 20
-print(now)
 goal_day = input("you should type the day ex) 1970-01-01 : ")
 
 goal_day_s = datetime.strptime(goal_day, "%Y-%m-%d")
 
 delta = goal_day_s - now_i
-#d_day = relativedelta(now, datetime(year, month, day))
 print( "%s day" %(delta.days))
+deltaToint = delta.days
+if deltaToint%3 == 0 :
+	day_time = deltaToint//3
+	night_time = deltaToint//3
+	first_night = deltaToint//3
+elif deltaToint%3 == 1 :
+	day_time = deltaToint//3 + 1
+	first_night = deltaToint//3 
+	night_time = deltaToint//3
+else :
+	day_time = deltaToint//3 + 1
+	first_night = deltaToint//3 + 1
+	night_time = deltaToint//3
 
-
+print(day_time) 
+print(" you have {}nighttime, {}firstnight, {}daytime".format(night_time,first_night,day_time))
+print(type(deltaToint))
